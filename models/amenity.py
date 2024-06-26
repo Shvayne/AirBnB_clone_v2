949a7d21c4ac681a54369d78dc5b35de741f8df8
Christiana Apinoko <122893327+Shvayne@users.noreply.github.com>
2024-06-26 22:37:50 +0100

    Add an instance of SQLAlchemy Table  for creating the many-to-many relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """defines an amenity class"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        from models.place import place_amenity
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
+    else:
+        name = ""
